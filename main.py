b733831facd7c2b0ef6e91a56b5bbdbc979b93d7
Oluwagbeminiyi <gagbedejobi@gmail.com>
2024-01-10 21:08:30 +0100

    Added a new intro function

--- main.py
+++ main.py
@@ -4,7 +4,22 @@
 WORD = "HANGMAN"
 
 
+def intro():
+    print(
+        """
+        _
+        | |
+        | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __
+        | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \\
+        | | | | (_| | | | | (_| | | | | | | (_| | | | |
+        |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                            __/ |
+                           |___/
+        """)
+
+
 def game():
+    intro()
     fails = 0
     while fails < 5:
         print(f"There are {len(WORD)} letters in this word.")
@@ -15,6 +30,11 @@ def game():
             print("fail")
             fails += 1
     print(f"The correct word is {WORD}")
+
+
+def catch_fails(fails):
+    if fails == 1:
+        print("")
 
 
 if __name__ == "__main__":
