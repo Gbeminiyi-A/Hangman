db4fcdda9eeb4c114c90584a66b33757300f5661
Oluwagbeminiyi <gagbedejobi@gmail.com>
2024-01-11 15:49:32 +0100

    Updated the user guess function to only exit after five fails and not five trials

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 
 WORD = "HANGMAN"
+guessed_word = "-" * len(WORD)
 
 HANGMAN_PICS = [r"""
   +--+
@@ -81,11 +82,9 @@ def intro():
 
 def game():
     intro()
-    fails = 0
-    print(f"There are {len(WORD)} letters in this word.")
-    while fails < 5:
-        user_guess()
-        fails += 1
+    print(f"There are {len(WORD)} letters in this word. \n")
+    user_guess()
+
     print(f"The correct word is {WORD}")
 
 
@@ -94,23 +93,31 @@ def catch_fails(fails):
 
 
 def user_guess():
-    letter = input("Enter a letter: ").upper()
-    if letter in WORD:
-        if letter in correct_guess or missed_letters:
-            print("You have entered this letter before, Try again")
+    fails = 0
+    while fails < 5:
+        letter = input("Enter a letter: ").upper()
+        if letter in WORD:
+            if letter in correct_guess:
+                print("You have entered this letter before, Try again")
+            else:
+                correct_guess.append(letter)
+                check_remaining(letter)
         else:
-            correct_guess.append(letter)
-            print(True)
-    else:
-        missed_letters.append(letter)
-    print(f"You've entered {', '.join(correct_guess)}, {', '.join(missed_letters)}")
+            if letter in missed_letters:
+                print("You have entered this letter before, Try again")
+            else:
+                missed_letters.append(letter)
+                fails += 1
+        print(f"You've entered {', '.join(correct_guess)}, {', '.join(missed_letters)}")
 
 
 def check_remaining(letter_input):
     position = []
-    for letter in WORD:
-        if letter_input == letter:
-            position.append(WORD.find(letter_input))
+    for i in WORD:
+        if letter_input == WORD:
+            position.append(i)
+            guessed_word.replace("-", letter_input)
+    print(guessed_word)
 
 
 if __name__ == "__main__":
