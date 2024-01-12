6ed43421d36857e9e67192c386878806011239db
Oluwagbeminiyi <gagbedejobi@gmail.com>
2024-01-12 10:04:28 +0100

    Updated the program to be more user friendly and added the ASCII art for each fail

--- main.py
+++ main.py
@@ -97,30 +97,30 @@ def catch_fails(fails):
 
 def user_guess():
     fails = 0
-    while fails < 5:
+    while fails < 7:
         letter = input("Enter a letter: ").upper()
         if letter in WORD:
             if letter in correct_guess:
-                print("You have entered this letter before, Try again")
+                print("You have entered this letter before, Try again\n")
             else:
                 correct_guess.append(letter)
                 show_guess(letter)
         else:
             if letter in missed_letters:
-                print("You have entered this letter before, Try again")
+                print("You have entered this letter before, Try again\n")
             else:
                 missed_letters.append(letter)
+                print("The letter you have entered is not in the word.")
+                print(HANGMAN_PICS[fails])
                 fails += 1
-        print(f"You've entered {', '.join(correct_guess)}, {', '.join(missed_letters)}")
+        print(f"You've entered {', '.join(correct_guess)}, {', '.join(missed_letters)}\n")
 
 
 def show_guess(letter_input):
     global guessed
-
     for i in word:
         if letter_input == i:
             index = word.index(i)
-
             guessed[index] = letter_input
     print("".join(guessed))
 
